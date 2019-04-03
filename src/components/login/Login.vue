5402abea4009802e6cd4e5c9f43817ab2af50696
dongbaowen <dongbaowen92@126.com>
2019-04-04 00:38:39 +0800

    登录表单

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,15 +1,23 @@
 <template>
   <div id="login-container">
-    <a-layout>
-      <a-layout-header>Header</a-layout-header>
-      <a-layout-content>Content</a-layout-content>
-      <a-layout-footer>Footer</a-layout-footer>
-    </a-layout>
-
-    <input id="username" v-model="username" placeholder="username">
-    <input id="password" v-model="password" placeholder="password">
-    <button @click="login" id="submit-login">登录</button>
-    <a-button type="primary">Button></a-button>
+    <a-row>
+      <a-col :span="8"></a-col>
+      <a-col :span="8">
+        <div id="login-form">
+          <div class="form-header"></div>
+          <a-form :form="form" @submit="handleSubmit">
+            <a-form-item label="账号" :label-col="{ span: 6 }" :wrapper-col="{ span: 13 ,offset: 1}">
+              <a-input/>
+            </a-form-item>  
+            <a-form-item label="密码" :label-col="{ span: 6 }" :wrapper-col="{ span: 13 ,offset: 1}">
+              <a-input/>
+            </a-form-item>
+            <a-button id="btn-submit" type="primary" html-type="submit">登录</a-button>
+          </a-form>
+        </div>
+      </a-col>
+      <a-col :span="8"></a-col>
+    </a-row>
   </div>
 </template>
 
@@ -19,11 +27,13 @@ import http from "../../common/http";
 export default {
   data() {
     return {
-      username: "",
-      password: ""
+      form: this.$form.createForm(this)
     };
   },
   methods: {
+    handleSubmit(e) {
+      alert(123);
+    },
     login() {
       http
         .post("http://localhost/api/user/login", { id: "ok" })
@@ -37,32 +47,36 @@ export default {
 </script>
 
 <style scoped>
+#login-form {
+  height: 500px;
+  margin: 60px 20px;
+  background-color: #fff;
+  border-radius: 2px;
+}
+#login-form .form-header {
+  padding-top: 30px;
+  height: 130px;
+}
+#btn-submit {
+  width: 300px;
+  height: 45px;
+  border-radius: 0px;
+  font-size: 16px;
+}
+
 #login-container {
   text-align: center;
+  height: 100%;
+  background-image: url("../../assets/sign_bg.db29b0fb.png");
+  background-repeat: no-repeat;
+  background-color: #b8e5f8;
+  background-size: cover;
+  width: 100%;
+  height: 100vh;
+  overflow: auto;
+  padding-right: 15px;
 }
-#login-container .ant-layout-header,
-#login-container .ant-layout-footer {
-  background: #7dbcea;
-  color: #fff;
-}
-#login-container .ant-layout-footer {
-  line-height: 1.5;
-}
-#login-container .ant-layout-sider {
-  background: #3ba0e9;
-  color: #fff;
-  line-height: 120px;
-}
-#login-container .ant-layout-content {
-  background: rgba(16, 142, 233, 1);
-  color: #fff;
-  min-height: 120px;
-  line-height: 120px;
-}
-#login-container > .ant-layout {
-  margin-bottom: 48px;
-}
-#login-container > .ant-layout:last-child {
-  margin: 0;
+#login-container .ant-row {
+  height: 100%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import { Button, Layout } from 'ant-design-vue'
+import { Button, Layout, Row, Col, Form, Input } from 'ant-design-vue'
 
 Vue.use(Button)
 Vue.use(Layout)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Form)
+Vue.use(Input)
 
 Vue.config.productionTip = false
 
