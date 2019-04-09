81e86e7e0fec573d75d54bd9ebba89c712c40b1a
dongbaowen <dongbaowen92@126.com>
2019-04-10 00:14:08 +0800

    register

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -3,18 +3,33 @@
     <a-row>
       <a-col :span="8"></a-col>
       <a-col :span="8">
-        <div id="login-form">
+        <div id="login-box">
           <div class="form-header"></div>
-          <a-form :form="form" @submit="handleSubmit">
-            <a-form-item label="账号" :label-col="{ span: 6 }" :wrapper-col="{ span: 13 ,offset: 1}">
-              <a-input/>
-            </a-form-item>  
-            <a-form-item label="密码" :label-col="{ span: 6 }" :wrapper-col="{ span: 13 ,offset: 1}">
-              <a-input/>
+
+          <a-form id="login-form" :form="form">
+            <a-form-item label="邮箱" :label-col="{ span: 6 }" :wrapper-col="{ span: 13 ,offset: 1}">
+              <a-input id="email" v-model="email"/>
             </a-form-item>
-            <a-button id="btn-submit" type="primary" html-type="submit">登录</a-button>
+            <a-form-item label="密码" :label-col="{ span: 6 }" :wrapper-col="{ span: 13 ,offset: 1}">
+              <a-input id="password" v-model="password"/>
+            </a-form-item>
+            <a-button class="btn-submit" id="btn-submit-login" @click="login" type="primary">登录</a-button>
+          </a-form>
+
+          <a-form id="register-form" :form="form">
+            <a-form-item label="邮箱" :label-col="{ span: 6 }" :wrapper-col="{ span: 13 ,offset: 1}">
+              <a-input id="userEmail" v-model="userEmail"/>
+            </a-form-item>
+            <a-form-item label="密码" :label-col="{ span: 6 }" :wrapper-col="{ span: 13 ,offset: 1}">
+              <a-input id="userPassword" v-model="userPassword"/>
+            </a-form-item>
+            <a-form-item label="昵称" :label-col="{ span: 6 }" :wrapper-col="{ span: 13 ,offset: 1}">
+              <a-input id="userNickname" v-model="userNickname"/>
+            </a-form-item>
+            <a-button class="btn-submit" id="btn-submit-rigister" @click="register" type="primary">注册</a-button>
           </a-form>
         </div>
+
       </a-col>
       <a-col :span="8"></a-col>
     </a-row>
@@ -27,16 +42,33 @@ import http from "../../common/http";
 export default {
   data() {
     return {
-      form: this.$form.createForm(this)
+      form: this.$form.createForm(this),
+      email: "",
+      password: "",
+      userEmail: "",
+      userPassword: "",
+      userNickname: ""
     };
   },
   methods: {
-    handleSubmit(e) {
-      alert(123);
-    },
     login() {
       http
-        .post("http://localhost/api/user/login", { id: "ok" })
+        .post("http://localhost/api/user/login", {
+          email: this.email,
+          password: this.password
+        })
+        .then(data => {
+          console.log("res: ", data);
+        })
+        .catch(err => console.log(err));
+    },
+    register() {
+      http
+        .post("http://localhost/api/user/register", {
+          userEmail: this.userEmail,
+          userPassword: this.userPassword,
+          userNickname: this.userNickname
+        })
         .then(data => {
           console.log("res: ", data);
         })
@@ -48,16 +80,19 @@ export default {
 
 <style scoped>
 #login-form {
+  display: none;
+}
+#login-box {
   height: 500px;
   margin: 60px 20px;
   background-color: #fff;
   border-radius: 2px;
 }
-#login-form .form-header {
+#login-box .form-header {
   padding-top: 30px;
   height: 130px;
 }
-#btn-submit {
+.btn-submit {
   width: 300px;
   height: 45px;
   border-radius: 0px;
