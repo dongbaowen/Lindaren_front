e541d33227c0c5628768693b83e8fa648ffdc8e5
baowen.dong <baowen.dong@qunar.com>
2019-04-12 18:09:28 +0800

    .

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -5,14 +5,16 @@
       <a-col :span="8">
         <div id="login-form">
           <div class="form-header"></div>
-          <a-form :form="form" @submit="handleSubmit">
-            <a-form-item label="账号" :label-col="{ span: 6 }" :wrapper-col="{ span: 13 ,offset: 1}">
-              <a-input/>
-            </a-form-item>  
-            <a-form-item label="密码" :label-col="{ span: 6 }" :wrapper-col="{ span: 13 ,offset: 1}">
-              <a-input/>
-            </a-form-item>
-            <a-button id="btn-submit" type="primary" html-type="submit">登录</a-button>
+          <a-form :form="form" @submit="login">
+            <div class="form-item-container">
+              <a-form-item label="邮箱">
+                <a-input id="email" v-model="email" placeholder="请填写邮箱"/>
+              </a-form-item>
+              <a-form-item label="密码">
+                <a-input id="password" v-model="password" placeholder="请填写密码"/>
+              </a-form-item>
+              <a-button id="btn-submit" type="primary" html-type="submit">登录</a-button>
+            </div>
           </a-form>
         </div>
       </a-col>
@@ -27,16 +29,21 @@ import http from "../../common/http";
 export default {
   data() {
     return {
-      form: this.$form.createForm(this)
+      form: this.$form.createForm(this),
+      email: "",
+      password: ""
     };
   },
   methods: {
     handleSubmit(e) {
-      alert(123);
+      console.log(this.email);
     },
     login() {
       http
-        .post("http://localhost/api/user/login", { id: "ok" })
+        .post("http://127.0.0.1/api/user/login", {
+          email: this.email,
+          password: this.password
+        })
         .then(data => {
           console.log("res: ", data);
         })
@@ -52,6 +59,7 @@ export default {
   margin: 60px 20px;
   background-color: #fff;
   border-radius: 2px;
+  padding: 0px 40px;
 }
 #login-form .form-header {
   padding-top: 30px;
@@ -59,11 +67,14 @@ export default {
 }
 #btn-submit {
   width: 300px;
-  height: 45px;
+  height: 40px;
   border-radius: 0px;
   font-size: 16px;
+  margin-top: 20px;
 }
-
+.form-item-container {
+  text-align: left;
+}
 #login-container {
   text-align: center;
   height: 100%;
